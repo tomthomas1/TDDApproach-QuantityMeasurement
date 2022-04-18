cd67be63af7d751d1f4c20a515e84ee3221b67cd
tomthomas1 <tomthomas8688@gmail.com>
2022-04-19 00:31:47 +0530

    Perform Inch test to check value for equality

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -114,5 +114,21 @@ public class QuantityMeasurementTest {
 	        Inch inch2 = new Inch(0);
 	        Assert.assertNotSame(inch1, inch2);
 	    }
+	 
+	 // TC 1.11 - Value check for equality
+	 @Test
+		public void given0Inchand0Inch_SameValue_ShouldReturnEqual() {
+			Inch inch1 = new Inch(0.0);
+			Inch inch2 = new Inch(0.0);
+			Assert.assertEquals(inch1, inch2);
+		}
+	 
+	// TC 1.11 - Value check for different values
+		 @Test
+			public void given0Inchand1Inch_DiffValue_ShouldReturnNotEqual() {
+				Inch inch1 = new Inch(0.0);
+				Inch inch2 = new Inch(1.0);
+				Assert.assertNotEquals(inch1, inch2);
+			}
 
 }
