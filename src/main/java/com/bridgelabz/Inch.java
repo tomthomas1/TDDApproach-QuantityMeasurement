4a0efe7746393447f9ea0d9657be64b6120439fd
tomthomas1 <tomthomas8688@gmail.com>
2022-04-19 00:20:35 +0530

    Perform Inch Test for Same 0 Inch Value

---
+++ src/main/java/com/bridgelabz/Inch.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class Inch {
+
+	private final double value;
+
+	public Inch(double value) {
+		this.value = value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Inch other = (Inch) obj;
+		if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+			return false;
+		return true;
+	}
+
+}
--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.bridgelabz.Feet;
+import com.bridgelabz.Inch;
 
 public class QuantityMeasurementTest {
 
@@ -22,46 +23,63 @@ public class QuantityMeasurementTest {
 		Feet feet2 = new Feet(1);
 		assertNotEquals(feet1, feet2);
 	}
-	
-	//TC 1.2 - Null Check
+
+	// TC 1.2 - Null Check
 	@Test
-    public void given0FeetandNullFeet_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(0);
-        Feet feet2 = null;
-        Assert.assertNotSame(feet1, feet2);
-    }
-	
-	//TC 1.3 - Reference Check
+	public void given0FeetandNullFeet_ShouldReturnNotEqual() {
+		Feet feet1 = new Feet(0);
+		Feet feet2 = null;
+		Assert.assertNotSame(feet1, feet2);
+	}
+
+	// TC 1.3 - Reference Check
 	@Test
-    public void givenFeetandFeetFromSameRef_WhenEqual_ShouldReturnTrue() {
-        Feet feet1 = new Feet(0);
-        boolean result = feet1 == feet1;
-        Assert.assertTrue(result);
-    }
-	
-	//TC 1.3 - Reference Check With Different values
+	public void givenFeetandFeetFromSameRef_WhenEqual_ShouldReturnTrue() {
+		Feet feet1 = new Feet(0);
+		boolean result = feet1 == feet1;
+		Assert.assertTrue(result);
+	}
+
+	// TC 1.3 - Reference Check With Different values
 	@Test
-    public void given0Feetand1FeetWithDiffRef_WhenNotEqual_ShouldReturnFalse() {
-        Feet feet1 = new Feet(0);
-        Feet feet2 = new Feet(1);
-        boolean result = feet1 == feet2;
-        Assert.assertFalse(result);
-    }
-	
+	public void given0Feetand1FeetWithDiffRef_WhenNotEqual_ShouldReturnFalse() {
+		Feet feet1 = new Feet(0);
+		Feet feet2 = new Feet(1);
+		boolean result = feet1 == feet2;
+		Assert.assertFalse(result);
+	}
+
 	// TC 1.4 - Type Check
 	@Test
-    public void given0Feetand0FeetFromDifferentType_ShouldReturnNotEqual() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0);
-        Assert.assertNotSame(feet1, feet2);
-    }
-	
+	public void given0Feetand0FeetFromDifferentType_ShouldReturnNotEqual() {
+		Feet feet1 = new Feet(0.0);
+		Feet feet2 = new Feet(0);
+		Assert.assertNotSame(feet1, feet2);
+	}
+
 	// TC 1.6 - Value check for equality.
-		@Test
-		public void given0Feetand0Feet_SameValue_ShouldReturnEqual() {
-			Feet feet1 = new Feet(1);
-			Feet feet2 = new Feet(1);
-			assertEquals(feet1, feet2);
-		}
+	@Test
+	public void given0Feetand0Feet_SameValue_ShouldReturnEqual() {
+		Feet feet1 = new Feet(1);
+		Feet feet2 = new Feet(1);
+		assertEquals(feet1, feet2);
+	}
+
+	// Inch Test Cases
+	// TC 1.7 - Same Inch value test.
+	@Test
+	public void given0Inchand0Inch_ShouldReturnEqual() {
+		Inch inch1 = new Inch(0.0);
+		Inch inch2 = new Inch(0.0);
+		Assert.assertEquals(inch1, inch2);
+	}
+	
+	// TC 1.7 - Different Inch value test.
+	@Test
+    public void given0Inchand1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotSame(inch1, inch2);
+    }
 
 }
